4e9c9d8991d9ede7f7b65cfb40da2b6e21f41194
arthurGC <aguzmanc@unsa.edu.pe>
2021-09-02 21:59:59 -0500

    Add pollution actions, actions creators, and pollutionReducer

---
+++ src/redux/pollution/pollution.js
@@ -0,0 +1,27 @@
+// Initial State
+const initialState = [];
+
+// Actions
+const INIT_POLLUTION = 'air_pollution/pollution/INIT_POLLUTION';
+
+// Action Creators
+export const addInitialPollution = () => async (dispatch) => {
+  const pollution = await getInitialPollution();
+  dispatch({
+    type: INIT_POLLUTION,
+    payload: pollution,
+  });
+};
+
+// Reducer
+const pollutionReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INIT_POLLUTION:
+      return [...action.payload];
+
+    default:
+      return state;
+  }
+};
+
+export default pollutionReducer;
