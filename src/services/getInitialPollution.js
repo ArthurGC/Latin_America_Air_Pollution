c5a91bb7e9a9bd686bcad4339125f768111d1dc0
arthurGC <aguzmanc@unsa.edu.pe>
2021-09-03 11:27:00 -0500

    Add link country images

--- src/services/getInitialPollution.js
+++ src/services/getInitialPollution.js
@@ -7,51 +7,61 @@ export const latinCountriesLatLong = [
     country: 'Peru',
     latitude: '-9.18',
     longitude: '-75.01',
+    link: '../../assets/Peru.png',
   },
   {
     country: 'Argentina',
     latitude: '-38.42',
     longitude: '-63.62',
+    link: '../../assets/Argentina.png',
   },
   {
     country: 'Bolivia',
     latitude: '-16.29',
     longitude: '-63.59',
+    link: '../../assets/Bolivia.png',
   },
   {
     country: 'Brasil',
     latitude: '-14.24',
     longitude: '-51.93',
+    link: '../../assets/Brasil.png',
   },
   {
     country: 'Chile',
     latitude: '-35.68',
     longitude: '-71.54',
+    link: '../../assets/Chile.png',
   },
   {
     country: 'Colombia',
     latitude: '4.57',
     longitude: '-74.30',
+    link: '../../assets/Colombia.png',
   },
   {
     country: 'Ecuador',
     latitude: '-1.83',
     longitude: '-78.18',
+    link: '../../assets/Ecuador.png',
   },
   {
     country: 'Paraguay',
     latitude: '-23.44',
     longitude: '-58.44',
+    link: '../../assets/Paraguay.png',
   },
   {
     country: 'Uruguay',
     latitude: '-32.52',
     longitude: '-55.77',
+    link: '../../assets/Uruguay.png',
   },
   {
     country: 'Venezuela',
     latitude: '6.42',
     longitude: '-66.59',
+    link: '../../assets/Venezuela.png',
   },
 ];
 
