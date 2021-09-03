21fdf6d5f48d09549ebcdb5ca865a10bd4554f0f
arthurGC <aguzmanc@unsa.edu.pe>
2021-09-02 23:27:31 -0500

    Add HomePage component

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Headline from './Headline';
+import MainCountry from './MainCountry';
+
+const HomePage = () => (
+  <>
+    <Headline />
+    <h2>Air Pollution by Country</h2>
+    <MainCountry />
+  </>
+);
+
+export default HomePage;
