8bbc0d221139585713c220c5d55067d4bd81da76
arthurGC <aguzmanc@unsa.edu.pe>
2021-09-03 12:45:31 -0500

    Add route to HomePage, NotMatch, and several DetailsPage

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,37 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Navbar from '../general/Navbar';
 import HomePage from './home/HomePage';
+import NotMatch from './home/NotMatch';
+import DetailsPage from './details/DetailsPage';
 
-const App = () => (
-  <Router>
-    <Navbar />
-    <Switch>
-      <Route>
-        <HomePage />
-      </Route>
-    </Switch>
-  </Router>
-);
+const App = () => {
+  const countries = useSelector((state) => state.pollution);
+
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        {countries.map((country) => (
+          <Route key={country.id} path={`/${country.name}`}>
+            <DetailsPage
+              name={country.name}
+              air={country.air}
+              components={country.components}
+            />
+          </Route>
+        ))}
+        <Route path="*">
+          <NotMatch />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
