f91a3eddd84a412c3b2bddba2590dbc1bbd7c0f4
arthurGC <aguzmanc@unsa.edu.pe>
2021-09-02 23:26:50 -0500

    Add Country component

---
+++ src/components/home/Country.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable global-require */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const Country = ({ name, air }) => (
+  <Link to="/country">
+    <div>
+      <img src={require(`../../assets/${name}.png`).default} alt={`${name}'s map`} />
+    </div>
+    <h2>{name}</h2>
+    <p>{air}</p>
+  </Link>
+);
+
+Country.propTypes = {
+  name: PropTypes.string.isRequired,
+  air: PropTypes.number.isRequired,
+};
+
+export default Country;
