6141a94bf3c30c6f5b7f1876c8c36e5ca0d681bf
arthurGC <aguzmanc@unsa.edu.pe>
2021-09-03 22:47:34 -0500

    Add test to Country component

--- src/__test__/components/Country.test.js
+++ src/__test__/components/Country.test.js
@@ -4,23 +4,46 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Country from '../../components/home/Country';
 
+const setRender = (props) => {
+  const component = render(<Router><Country name={props.name} air={props.air} /></Router>);
+  return component;
+};
+
 describe('Test Country component', () => {
   // Arrange
-  const props = {
-    name: 'Peru',
-    air: 5,
-  };
-    // Act
-  const component = render(
-    <Router>
-      <Country name={props.name} air={props.air} />
-    </Router>,
-  );
+  // Act
+  let component;
+  beforeEach(() => {
+    const props = {
+      name: 'Peru',
+      air: '5',
+    };
+    component = setRender(props);
+  });
 
   // Assert
-  test('Testing Country component render', () => {
-    component.getByText('Peru');
-    component.getByText(props.air);
-    component.getByRole('img', { name: `${props.name}'s map` });
+  test('Should render without errors', () => {
+    const { getByTestId } = component;
+    expect(getByTestId('countryCategory')).toBeInTheDocument();
+  });
+
+  test('Should render a title element', () => {
+    const { getByTestId } = component;
+    expect(getByTestId('countryTitle')).toBeInTheDocument();
+  });
+
+  test('Should render a title value', () => {
+    const { getByText } = component;
+    expect(getByText('Peru')).toBeInTheDocument();
+  });
+
+  test('Should render a air quality element', () => {
+    const { getByTestId } = component;
+    expect(getByTestId('countryAirQuality')).toBeInTheDocument();
+  });
+
+  test('Should render a air quality value', () => {
+    const { getByText } = component;
+    expect(getByText('5')).toBeInTheDocument();
   });
 });
