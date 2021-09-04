6dc78142d98be6cd8a67946d1e76ac9f832c5b17
arthurGC <aguzmanc@unsa.edu.pe>
2021-09-03 22:50:05 -0500

    Remove logger

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,5 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import pollutionReducer from './pollution/pollution';
 
@@ -10,7 +9,7 @@ const reducer = combineReducers({
 
 const store = createStore(
   reducer,
-  composeWithDevTools(applyMiddleware(logger, thunk)),
+  composeWithDevTools(applyMiddleware(thunk)),
 );
 
 export default store;
