6a0145fa0a9768cf82cabbd17f9458d01d09d7e9
arthurGC <aguzmanc@unsa.edu.pe>
2021-09-05 16:50:55 -0500

    Add styles to Headline component

--- src/components/home/Headline.js
+++ src/components/home/Headline.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Latin from '../../assets/Latin.png';
 
 const Headline = () => (
-  <div className="bg-dribble px-8">
-    <div data-testid="headlineSection" className="container mx-auto">
-      <div>
-        <img src={Latin} alt="Latin America map" />
+  <div className="bg-dribble px-6 py-2">
+    <div data-testid="headlineSection" className="container mx-auto flex items-center justify-evenly gap-4">
+      <div className="flex-1">
+        <img src={Latin} alt="Latin America map" className="opacity-30 max-h-40" />
       </div>
-      <h2 data-testid="headlineTitle">Latin America</h2>
+      <div className="flex-1">
+        <h2 data-testid="headlineTitle" className="font-lato font-black text-xl text-white text-right uppercase">Latin America</h2>
+        <p className="font-lato font-normal text-base text-white text-right">Air quality index 3</p>
+      </div>
     </div>
   </div>
 );
