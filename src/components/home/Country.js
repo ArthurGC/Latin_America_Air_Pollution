50fb11389078d946b73e7123f77883e13b7ff3aa
arthurGC <aguzmanc@unsa.edu.pe>
2021-09-05 16:50:37 -0500

    Add styles to Country component

--- src/components/home/Country.js
+++ src/components/home/Country.js
@@ -5,12 +5,14 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const Country = ({ name, air }) => (
-  <Link to={`/${name}`}>
-    <div data-testid="countryCategory">
-      <img src={require(`../../assets/${name}.png`).default} alt={`${name}'s map`} />
+  <Link to={`/${name}`} className="bg-dribble flex flex-col items-center gap-4 p-4">
+    <div data-testid="countryCategory" className="">
+      <img src={require(`../../assets/${name}.png`).default} alt={`${name}'s map`} className="opacity-30 max-h-28" />
     </div>
-    <h2 data-testid="countryTitle">{name}</h2>
-    <p data-testid="countryAirQuality">{air}</p>
+    <div className="min-w-full">
+      <h2 data-testid="countryTitle" className="font-lato font-black text-sm text-white text-right uppercase">{name}</h2>
+      <p data-testid="countryAirQuality" className="font-lato font-normal text-base text-white text-right">{`AQI: ${air}`}</p>
+    </div>
   </Link>
 );
 
