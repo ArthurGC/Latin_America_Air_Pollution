07e4add41844a414283e23bbd93e566efdabbe11
arthurGC <aguzmanc@unsa.edu.pe>
2021-09-05 16:52:14 -0500

    Fix styles to Navbar component

--- src/general/Navbar.js
+++ src/general/Navbar.js
@@ -6,7 +6,7 @@ import usePollution from '../hooks/usePollution';
 const Navbar = () => {
   usePollution();
   return (
-    <header className="bg-dribble px-8 py-5">
+    <header className="bg-dribble px-6 py-3">
       <nav data-testid="navbarContainer" className="container mx-auto grid grid-cols-6">
         <Link to="/" className="text-white justify-self-start"><FaAngleLeft className="h-7" /></Link>
         <p data-testid="navbarTitle" className="font-gill text-xl text-white col-span-4 place-self-center pt-1">Latin&#39;s Air Pollution</p>
