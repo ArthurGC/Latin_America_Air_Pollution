87b68a24b7b4a29c4cbfbe325237915e3d0712d3
arthurGC <aguzmanc@unsa.edu.pe>
2021-09-06 10:42:05 -0500

    Fix background on Headline component

--- src/components/home/Headline.js
+++ src/components/home/Headline.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Latin from '../../assets/Latin.png';
 
 const Headline = () => (
-  <div className="bg-dribble px-6 py-2">
+  <div className="bg-head px-6 py-2">
     <div data-testid="headlineSection" className="container mx-auto flex items-center justify-center gap-4">
       <div className="flex-1">
         <img src={Latin} alt="Latin America map" className="opacity-30 max-h-32 mx-auto" />
