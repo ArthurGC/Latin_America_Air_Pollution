28c0ae55981220ed9ab224e61a1e7c01e3763228
arthurGC <aguzmanc@unsa.edu.pe>
2021-09-06 10:43:09 -0500

    Fix background and text of air pollution components

--- src/components/details/DetailsPage.js
+++ src/components/details/DetailsPage.js
@@ -2,46 +2,48 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const DetailsPage = ({ name, air, components }) => (
-  <div>
-    <div>
-      <h2>{name}</h2>
-      <p>{air}</p>
+  <div className="bg-dribble py-1 min-h-screen">
+    <div className="">
+      <div className="bg-head py-4">
+        <h2 className="font-lato font-black text-xl text-white text-center uppercase">{name}</h2>
+        <p className="font-lato font-normal text-base text-white text-center">{`Air quality index: ${air}`}</p>
+      </div>
+      <p className="bg-category font-lato text-sm text-white uppercase px-6 py-2">Air&#39;s components:</p>
+      <ul>
+        <li className="flex justify-between px-6 py-4 row">
+          <p className="font-lato font-bold text-base text-white">Co</p>
+          <p className="font-lato font-normal text-base text-white">{`${components.co} μg/m3`}</p>
+        </li>
+        <li className="flex justify-between px-6 py-4 row">
+          <p className="font-lato font-bold text-base text-white">No</p>
+          <p className="font-lato font-normal text-base text-white">{`${components.no} μg/m3`}</p>
+        </li>
+        <li className="flex justify-between px-6 py-4 row">
+          <p className="font-lato font-bold text-base text-white">No2</p>
+          <p className="font-lato font-normal text-base text-white">{`${components.no2} μg/m3`}</p>
+        </li>
+        <li className="flex justify-between px-6 py-4 row">
+          <p className="font-lato font-bold text-base text-white">O3</p>
+          <p className="font-lato font-normal text-base text-white">{`${components.o3} μg/m3`}</p>
+        </li>
+        <li className="flex justify-between px-6 py-4 row">
+          <p className="font-lato font-bold text-base text-white">SO2</p>
+          <p className="font-lato font-normal text-base text-white">{`${components.so2} μg/m3`}</p>
+        </li>
+        <li className="flex justify-between px-6 py-4 row">
+          <p className="font-lato font-bold text-base text-white">PM25</p>
+          <p className="font-lato font-normal text-base text-white">{`${components.pm2_5} μg/m3`}</p>
+        </li>
+        <li className="flex justify-between px-6 py-4 row">
+          <p className="font-lato font-bold text-base text-white">PM10</p>
+          <p className="font-lato font-normal text-base text-white">{`${components.pm10} μg/m3`}</p>
+        </li>
+        <li className="flex justify-between px-6 py-4 row">
+          <p className="font-lato font-bold text-base text-white">NH3</p>
+          <p className="font-lato font-normal text-base text-white">{`${components.nh3} μg/m3`}</p>
+        </li>
+      </ul>
     </div>
-    <p>Air&#39;s components</p>
-    <ul>
-      <li>
-        <p>Co</p>
-        <p>{components.co}</p>
-      </li>
-      <li>
-        <p>No</p>
-        <p>{components.no}</p>
-      </li>
-      <li>
-        <p>No2</p>
-        <p>{components.no2}</p>
-      </li>
-      <li>
-        <p>O3</p>
-        <p>{components.o3}</p>
-      </li>
-      <li>
-        <p>SO2</p>
-        <p>{components.so2}</p>
-      </li>
-      <li>
-        <p>PM25</p>
-        <p>{components.pm2_5}</p>
-      </li>
-      <li>
-        <p>PM10</p>
-        <p>{components.pm10}</p>
-      </li>
-      <li>
-        <p>NH3</p>
-        <p>{components.nh3}</p>
-      </li>
-    </ul>
   </div>
 );
 
