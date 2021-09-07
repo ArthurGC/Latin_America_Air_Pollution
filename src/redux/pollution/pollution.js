c673abce4c88f62726b117e00e26a117f56cc1db
arthurGC <aguzmanc@unsa.edu.pe>
2021-09-07 12:00:30 -0500

    Add actions and actions creators to filter data

--- src/redux/pollution/pollution.js
+++ src/redux/pollution/pollution.js
@@ -1,10 +1,15 @@
 import getInitialPollution, { refactorData } from '../../services/getInitialPollution';
 
 // Initial State
-const initialState = [];
+const initialState = {
+  countries: [],
+  filtered: [],
+  selectStatus: 'All',
+};
 
 // Actions
 export const INIT_POLLUTION = 'air_pollution/pollution/INIT_POLLUTION';
+export const FILTER_POLLUTION = 'air_pollution/pollution/FILTER_POLLUTION';
 
 // Action Creators
 export const addInitialPollution = () => async (dispatch) => {
@@ -16,12 +21,33 @@ export const addInitialPollution = () => async (dispatch) => {
   });
 };
 
+export const sendFilter = (option) => ({
+  type: FILTER_POLLUTION,
+  payload: option,
+});
+
+export const filterState = (state, option) => {
+  let filterStore;
+  if (option !== 'All') {
+    filterStore = state.countries.filter((country) => country.air === option);
+  } else {
+    filterStore = state.countries;
+  }
+  return filterStore;
+};
+
 // Reducer
 const pollutionReducer = (state = initialState, action) => {
   switch (action.type) {
     case INIT_POLLUTION:
-      return action.payload;
+      return { ...state, countries: action.payload, filtered: action.payload };
 
+    case FILTER_POLLUTION:
+      return {
+        ...state,
+        filtered: filterState(state, action.payload),
+        selectStatus: action.payload,
+      };
     default:
       return state;
   }
